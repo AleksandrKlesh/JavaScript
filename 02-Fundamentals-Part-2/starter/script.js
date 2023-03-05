6f3e8d472623fa8d4e30f5b806a71944b00185b2
Aleksandr Kleshchev <alex@Aleksandrs-MacBook-Pro.local>
2023-03-06 00:19:55 +0700

    fruit-juice function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriverLicense = true;
@@ -9,3 +9,27 @@ if (hasDriversLicense) console.log('I can drive!');
 const interface = 'Audio';
 const private = 123;
 const if = 321;
+*/
+//Functions
+
+function logger() {
+    console.log("Hi! I'm great!")
+};
+
+//running / calling / invoking function
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `There are ${apples} apples and ${oranges} oranges`;
+    return juice;
+};
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
