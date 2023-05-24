c6a6db212a0fdb97c72e3b563d2885088cb67aed
AleksandrKlesh <alex73kl@gmail.com>
2023-05-24 16:01:31 +0700

    functions inside functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -70,13 +70,54 @@ const num = Number('23');
 
 //  console.log(yearsUntilRetirement(1991));
 
-//several parameters
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`;
- }
+// //several parameters
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+//  }
 
- console.log(yearsUntilRetirement(1991, 'Alex'));
- console.log(yearsUntilRetirement(1980, 'Bob'));
+//  console.log(yearsUntilRetirement(1991, 'Alex'));
+//  console.log(yearsUntilRetirement(1980, 'Bob'));
+//  console.log(12)
+
+// // Functions calling other functions
+
+// function cutFruitPieces (fruit) {
+//     return fruit * 4;
+// };
+
+// function fruitProcessor (apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+//     console.log(juice);
+// };
+
+// fruitProcessor(2,3);
+
+
+
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+// if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+// } else {
+//     console.log(`${firstName}'s alredy retired`);
+//     return -1;
+// };
+// } 
+
+// console.log(yearsUntilRetirement(1990, 'Alex'));
+// console.log(yearsUntilRetirement(1970, 'Jim'));
+
