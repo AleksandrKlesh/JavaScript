85ed2b082ad541ed8fe52b9a50c87281271d8ccf
AleksandrKlesh <alex73kl@gmail.com>
2023-09-07 23:46:32 +0700

    the game works, need refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,10 +16,10 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -32,12 +32,17 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (highScore < score) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too high!';
-      document.querySelector('.score').textContent = score;
       score--;
+      document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent =
         'You lost the game((9(99';
@@ -47,12 +52,23 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too low!';
-      document.querySelector('.score').textContent = score;
       score--;
+      document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent =
         'You lost the game((9(99';
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '';
 });
