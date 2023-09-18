d8d37eadf70daead001e71e06bfb00647763fddd
AleksandrKlesh <alex73kl@gmail.com>
2023-09-18 16:24:59 +0700

    added functionality for rollDice btn

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,22 +1,27 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const current0El = document.querySelector('#current--0');
-const current1El = document.getElementById('current--1');
-let currentScore = 0;
 
 // Starting conditions
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 const diceRoll = () => Math.trunc(Math.random() * 6 + 1);
 
@@ -33,10 +38,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice; // CHANGE LATER
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switching to next player
-    current0El.textContent = 0;
-    console.log('The opponent turn!');
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // Ternary operator
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
   }
 });
