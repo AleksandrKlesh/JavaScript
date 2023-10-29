dec75c6ef7bf119dd97dc6353050254e07b78c9f
AleksandrKlesh <alex73kl@gmail.com>
2023-10-29 23:40:22 +0700

    more competent approach

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1039,23 +1039,39 @@ document.querySelector('button').addEventListener('click', function () {
 //   🔴 Delayed Arrival from HEL to FAO (12h05)
 //            Departure from FAO to LIS (12h30)
 
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+////////////////////////// My solution /////////////////////////
+
+// const flightsInfo = flights.split('+');
+// for (const [i, flight] of flightsInfo.entries()) {
+//   const info = flight.trim().split(';');
+//   const str = info[0].replaceAll('_', ' ');
+//   const strUpper = str.split(' ');
+
+//   const goFrom = info[1].slice(0, 3).toUpperCase();
+//   const goTo = info[2].slice(0, 3).toUpperCase();
+//   const time = info[3];
+//   const output = `${str.startsWith(' Delayed') ? '🔴' : ''}${
+//     str + ' '
+//   }from ${goFrom} to ${goTo} (${time.replace(':', 'h')})`.padStart(50);
+//   console.log(output);
+// }
+
+///////////////////////// Different solution /////////////////////////
+
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-////////////////////////// My solution /////////////////////////
-const flightsInfo = flights.split('+');
-for (const [i, flight] of flightsInfo.entries()) {
-  const info = flight.trim().split(';');
-  const str = info[0].replaceAll('_', ' ');
-  const strUpper = str.split(' ');
+const getCode = str => str.slice(0, 3).toUpperCase();
 
-  const goFrom = info[1].slice(0, 3).toUpperCase();
-  const goTo = info[2].slice(0, 3).toUpperCase();
-  const time = info[3];
-  const output = `${str.startsWith(' Delayed') ? '🔴' : ''}${
-    str + ' '
-  }from ${goFrom} to ${goTo} (${time.replace(':', 'h')})`.padStart(50);
-  console.log(output);
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replace(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} ${time.replace(':', 'h')}`;
+  console.log(output.padStart(35));
 }
-
-///////////////////////// Different solution /////////////////////////
